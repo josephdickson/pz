aecc10e7a9fe0397d84b99072fc9c78502a2b33d
Joseph Dickson <joseph.e.dickson@gmmail.com>
2014-11-18 14:45:00 -0800

    Contact Information
    Added to Options page so the information can be edited within wordpress.

--- Pitzer-College/sidebar-contact-information.php
+++ Pitzer-College/sidebar-contact-information.php
@@ -1,9 +1,24 @@
+<?php
+	// Footer Contact Information
+	$office = get_field('office' , 'option');
+	$location = get_field('location' , 'option');
+	$phone = get_field('phone' , 'option');
+	$address = get_field('address' , 'option');
+	$email = get_field('email' , 'option');
+
+ ?>
+
 <ul>
     <li>Contact us</li>
-    <li><strong>Office of Communications</strong></li>
-    <li class="small">Mead 130</li>
-    <li class="small">909.621.8219</li>
-    <li class="small">Fax: 909.621.8798</li>
-    <li class="small">1050 N. Mills Avenue</li>
-    <li class="small">Claremont, CA 91711</li>
+    <li><strong class="text-orange"><?php if( $email ) : 
+		
+		echo '<a href="mailto:' . $email . '">' . $office . '</a>'; 
+	else :
+		echo $office; 
+	endif;		
+
+	?></strong></li>
+    <li class="small"><?php echo $location; ?></li>
+    <li class="small"><?php echo $phone; ?></li>
+    <li class="small"><?php echo $address; ?></li>
 </ul>
